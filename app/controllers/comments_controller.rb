d1d0a27d90b38665f39a8b9c28100e18e92be715
Your Name <moi@moi.moi>
2014-12-23 15:05:59 +0100

    added some css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,7 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
   #Create comments
   def create
-    @comment = Comment.new(comment_params)
+    #@comment = Comment.new(comment_params)
     @post  = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
@@ -32,11 +19,6 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:author, :body, :post_id)
