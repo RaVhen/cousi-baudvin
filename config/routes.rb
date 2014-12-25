06bd24ce6d72881cfa0454620c71b3b570e30b40
Your Name <moi@moi.moi>
2014-12-25 11:30:42 +0100

    Added sign in with devise. Default user:moi@moi.com and pass:superuser

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+  #devise_for :users
+  devise_for :users do
+    delete 'logout' => 'sessions#destroy', :as => :destroy_user_session
+    get 'login' => 'devise/sessions#new'
+  end
+  
   resources :posts do
     resources :comments
   end
+
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
